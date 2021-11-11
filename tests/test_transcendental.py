a8e0db9c410a384ae4b4d712f7e66672ce7f5776
Gustavo Moura <gustavorobert92@gmail.com>
2021-11-11 20:49:08 -0300

    Added Github Workflow file configuration and an unit test case for 'Service Transcendental' using pytest

---
+++ tests/test_transcendental.py
@@ -0,0 +1,11 @@
+import requests as req
+
+def getFatorial_calc(n1):
+    param = {'arg1': int(n1)}
+    res = req.get("http://localhost:5002/fatorial", param)
+    data = res.json()
+
+    return data["result"]
+
+def test_function_soma():
+    assert getFatorial_calc(5) == 120
